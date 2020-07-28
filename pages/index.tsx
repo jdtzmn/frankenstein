9ffcd0f73958b4b5263f62c885060e934ebab486
jdtzmn <jdtzmn@gmail.com>
2020-07-27 21:01:26 -0400

    Add project description to the homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,9 @@ function Index () {
         <title>Frankenstein Experiment</title>
       </Head>
       <h1>Frankenstein Experiment</h1>
+      <p>An experiment with <a href="https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver">MutationObserver</a></p>
+      <p>Modifying the DOM on the creation page updates the webpage for everyone</p>
+      <br/>
       <Link href="/monster">
         <a>View the creation</a>
       </Link>
